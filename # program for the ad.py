cd8a955ef5140c946d8da84cd530155c10957170
RiazAdil <43162412+RiazAdil@users.noreply.github.com>
2018-09-11 10:40:06 +0530

    Add files via upload

---
+++ # program for the ad.py
@@ -0,0 +1,5 @@
+#program for the addition of two numbers
+a=int(input("Enter the value of A :"))
+b=int(input("Enter the value of B :"))
+c=a+b
+print (c)
---
+++ ifelse.py
@@ -0,0 +1,12 @@
+#program for complex calculation of numbers
+
+
+
+a=int(input("Enter the value of A :"))
+b=int(input("Enter the value of B :"))
+print("A :",a)
+print("B :",b)
+if(a>b):
+	print(a,"Is greater")
+else:
+    print(b,"Is greater")	
